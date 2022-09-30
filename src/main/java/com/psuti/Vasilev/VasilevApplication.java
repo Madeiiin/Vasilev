4108bcf3f034351d363d1a14b6a4188e80ceebde
Madeiiin <105726989+Madeiiin@users.noreply.github.com>
2022-09-30 21:17:36 +0400

    Initial commit

---
+++ src/main/java/com/psuti/Vasilev/VasilevApplication.java
@@ -0,0 +1,13 @@
+package com.psuti.Vasilev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasilevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasilevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psuti/Vasilev/VasilevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psuti.Vasilev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasilevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
